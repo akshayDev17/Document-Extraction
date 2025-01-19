51f14adc9311fa1979e8f0920446dae2e543412c
Akshay Prabhakant <akshayprabhakant@gmail.com>
2025-01-19 11:42:55 +0530

    cleaned out to create a new directory structure

--- pdfToText.py
+++
@@ -1,7 +0,0 @@
-import pdftotext
-
-def convert(pdf_fname, output_fname):
-    with open(pdf_fname, "rb") as f1:
-        pdf = pdftotext.PDF(f1)
-    with open(output_fname, "w+") as f2:
-        f2.write("\n\n".join(pdf))
